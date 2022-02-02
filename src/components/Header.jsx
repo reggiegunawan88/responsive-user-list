b5fec419776cbfcf86ec332586b109f6c32e5225
reggiegunawan <reggiegunawan88@gmail.com>
2022-02-02 11:38:26 +0700

    feat: trigger filtering function via Context

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,33 @@
+import { useContext } from "react";
+import { UserListContext } from "App";
+
 function Header() {
+  const userCtx = useContext(UserListContext);
+
+  const filterColor = () => {
+    userCtx.filterColor();
+  };
+
+  const filterCities = () => {
+    userCtx.filterCities();
+  };
+
   return (
     <div className="flex flex-row justify-between">
-      <h3>Qoala Apps</h3>
-      <div className="flex flex-row gap-x-3">
-        <button>Color</button>
-        <button>Cities</button>
+      <div className="flex flex-row items-center gap-x-3">
+        <img alt="logo" src="/qoala-logo.png" className="w-9 h-9 rounded" />
+        <h3 className="uppercase font-serif">Qoala Apps</h3>
+      </div>
+      <div className="flex flex-col laptopM:flex-row gap-4 items-center">
+        <p>Filter by:</p>
+        <div className="flex flex-row gap-x-3">
+          <button onClick={filterColor} className="btn-filter">
+            Color
+          </button>
+          <button onClick={filterCities} className="btn-filter">
+            Cities
+          </button>
+        </div>
       </div>
     </div>
   );
