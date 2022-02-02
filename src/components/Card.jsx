a32414c5a0e815eb730839757a238b9c3693f462
reggiegunawan <reggiegunawan88@gmail.com>
2022-02-02 12:16:29 +0700

    feat: conditional color for bg card user

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,23 @@
 function Card(props) {
   const userData = props.user;
+
+  // conditional bg color based on user's age
+  const bgColor = () => {
+    const age = props.user.dob.age;
+    if (age < 31) {
+      return "lightcoral";
+    }
+    if (age >= 31 && age < 56) {
+      return "lightgreen";
+    }
+    if (age >= 56) {
+      return "skyblue";
+    }
+    return "";
+  };
+
   return (
-    <div style={{ width: "350px" }} className="border-default border-black p-3 rounded-md shadow-lg">
+    <div style={{ width: "350px", backgroundColor: bgColor() }} className="border-default border-black p-3 rounded-md shadow-lg">
       <div className="flex flex-col gap-y-4 text-center">
         <div className="flex flex-row laptopM:flex-col gap-x-4 gap-y-4 items-center justify-evenly">
           <img alt="user-pict" src={userData.picture.thumbnail} className=" w-20 h-20" />
